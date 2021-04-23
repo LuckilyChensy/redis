71cae532d4ac34d02d207ca2df97787d0ba5b447
SoSo <LuckilyChensy@163.com>
2021-04-23 23:54:27 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/redis-cli.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/adlist.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/adlist.c.o"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/ae.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/ae.c.o"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/anet.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/anet.c.o"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/crc64.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/crc64.c.o"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/redis-cli.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/redis-cli.c.o"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/release.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/release.c.o"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/sds.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/sds.c.o"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/src/zmalloc.c" "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/CMakeFiles/redis-cli.dir/src/zmalloc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../deps/hiredis"
+  "../deps/linenoise"
+  "../deps/lua/src"
+  "../deps/hiredis/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/deps/linenoise/CMakeFiles/linenoise.dir/DependInfo.cmake"
+  "/cygdrive/d/WorkSpace/Clion/redis6/redis/cmake-build-debug/deps/hiredis/CMakeFiles/hiredis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
